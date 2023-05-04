26b619ffb16a3fdf4fe15c23bc08bd7c497c093b
Antonio <a.hortalsegura@gmail.com>
2023-05-04 12:28:06 +0200

    Add methods to insert text and move cursor

--- +weblab/+components/CodeEditor.m
+++ +weblab/+components/CodeEditor.m
@@ -9,6 +9,14 @@ classdef CodeEditor < weblab.internal.FrameComponent & ...
         % THEME: Theme used in the editor
         Theme (1,1) string {mustBeMember(Theme, ["light","dark"])} = "light";
 
+        % EDITABLE: Whether the user can write text in the editor
+        Editable (1,1) logical = true;
+
+        % WRAPLINES: If a line exceeds the width of the editor, break it into
+        % more lines (not counted as a new line number). If false, scroll to see
+        % the overflow
+        WrapLines (1,1) logical = false;
+
     end
 
     events (Description = "HasCallbackProperty")
@@ -19,10 +27,49 @@ classdef CodeEditor < weblab.internal.FrameComponent & ...
 
     methods 
         function this = CodeEditor()
+            % CODEEDITOR constructor
             addlistener(this, "ValueChanged", ...
                 @(~,e) this.changePropertiesWithoutNotifyingView("Value", e.Data));
+        end
+
+        function insertText(this, text, cursorPosition)
+            % INSERTTEXT writes the input text into the editor at the specified
+            % cursor position. If no position is specified, the text is appended
+            % at the current cursor position
+            arguments
+                this (1,1) weblab.components.CodeEditor
+                text (1,1) string
+                cursorPosition (1,1) {mustBeCursorPosition} = "current";
+            end
+
+            % Numbers in js start from 0
+            if isnumeric(cursorPosition)
+                cursorPosition = cursorPosition - 1;
+            end
+            data = struct("text", text, "pos", cursorPosition);
+            this.publish(weblab.event.Event("insert_text", data));
+        end
+
+        function moveCursor(this, cursorPosition) 
+            % MOVECURSOR positions the cursor at the specified location
+            arguments
+                this (1,1) weblab.components.CodeEditor
+                cursorPosition (1,1) {mustBeCursorPosition}
+            end
+            this.publish(weblab.event.Event("move_cursor", cursorPosition));
         end
     end
 
 
+end
+
+
+function mustBeCursorPosition(position) 
+% MUSTBECURSORPOSITION checks that the specified position is one of the
+% following;
+% - An integer number greater than 0 (start = 1)
+% - One of the following: "start", "end", "current"
+
+assert((isinteger(position) && position > 0 ) || ...
+    any(strcmp(position, ["start","end","current"])));
 end
--- +weblab/source/src/+components/codeeditor/src/CodeEditor.svelte
+++ +weblab/source/src/+components/codeeditor/src/CodeEditor.svelte
@@ -1,20 +1,36 @@
 <script lang="ts">
   import CodeMirror from "svelte-codemirror-editor";
   import { StreamLanguage } from "@codemirror/language";
+  import { ViewPlugin } from "@codemirror/view";
+  import type { EditorView } from "@codemirror/view";
+
   import darkTheme from "./theme/dark";
   import lightTheme from "./theme/light";
   import matlab from "./lang/matlab";
 
   export let theme: string = "light";
   export let value: string = "";
+  export let editable: boolean = true;
+  export let wrap_lines: boolean = false;
+
+  export let _view: EditorView;
 
   const matlab_lan = StreamLanguage.define(matlab);
+  const extensions = [
+    matlab_lan, 
+    ViewPlugin.define((v) => {
+      _view = v;
+      return {};
+    }),
+  ];
 </script>
 
 <CodeMirror
-  extensions={[matlab_lan]}
+  {extensions}
   tabSize={4}
   theme={theme === "dark" ? darkTheme : lightTheme}
   {value}
+  {editable}
+  lineWrapping={wrap_lines}
   on:change
 />
