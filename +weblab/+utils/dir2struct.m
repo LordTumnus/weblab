e8269c81aac6ae1bd00e1412b2c982b839a3a232
Antonio <a.hortalsegura@gmail.com>
2023-05-16 09:18:40 +0200

    show icons in dir2struct

--- +weblab/+utils/dir2struct.m
+++ +weblab/+utils/dir2struct.m
@@ -1,4 +1,4 @@
-function root = dir2struct(folder)
+function [root, iconMap] = dir2struct(folder)
 % DIR2STRUCT converts a folder into a nested struct() that can be used inside a
 % weblab tree component
 % Usage:
@@ -11,6 +11,7 @@ root = struct("name", "root");
 root.children = {};
 
 root = rAdd(root, folder);
+iconMap = getIcons();
 
     function node = rAdd(node, folder)
         % RADD recursively add contents to struct node
@@ -23,9 +24,36 @@ root = rAdd(root, folder);
             nn.children = {};
             if (fn(ii).isdir)
                 nn = rAdd(nn, fullfile(folder,fn(ii).name));
+            else
+                [~,~,itype] = fileparts(fn(ii).name);
+                nn.icon = strrep(itype,".","");
             end
             node.children{end + 1} = nn ;
         end
-
     end
+
 end
+
+
+function iconMap = getIcons()
+% GETICONS returns a struct mapping file types to a weblab.style.Image
+% Icons are located in ../resources/file_icons
+
+iconDir = fullfile(fileparts(mfilename("fullpath")), "..", "resources", "file_icons");
+iconFiles = dir(iconDir);
+iconNames = strrep(string({iconFiles.name}),".png","");
+iconNames(1:2) = [];
+
+iconMap = struct();
+for ii = 1:numel(iconNames)
+    iconMap.(iconNames(ii)) = weblab.style.Image(fullfile(iconDir, iconNames(ii)+".png"));
+end
+
+% Also add a default icon that can be used as a backup when the file doesnt
+% match any of the icon types
+iconMap.default = weblab.style.Image(fullfile(iconDir,"file.png"));
+
+
+end
+
+
---
+++ +weblab/resources/readme.md
@@ -0,0 +1,1 @@
+<a href="https://www.flaticon.com/free-icons/js" title="js icons">File icons created by Dimitry Miroliubov - Flaticon</a>
